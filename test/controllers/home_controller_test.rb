f4e94b7235684a715e2e228e65215a393096612c
soir20 <71418127+soir20@users.noreply.github.com>
2022-01-19 14:01:23 -0500

    Fix incorrect root URL in test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get landing page" do
+    get root_url
     assert_response :success
   end
 end
